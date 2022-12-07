eeff141d7062af9e73644a2873a4d472c2f7dfc0
azizsenturk <aziz.senturk@gmail.com>
2022-12-07 18:22:42 +0300

    Test

--- src/@theme/index.jsx
+++ src/@theme/index.jsx
@@ -3,4 +3,4 @@ export { default as LanguageSwitcher } from './LanguageSwitcher';
 export { default as PageLayout } from './PageLayout';
 export { default as PageTemplate } from './PageTemplate';
 export { default as SvgIcoMoon } from './SvgIcoMoon';
-export { default as Utils } from './Utils';
+export { default as Utils } from './Utils'; 
